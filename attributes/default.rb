a1bcd975a5cc04e320cb6c742c8dfc2b2b1dff19
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-10-08 15:29:03 -1000

    Modification importante :
    - création de nouveau attributs
    - ajout des addresses de classe privée dans qmail-smtpd.rules
    - configuration de courier-imap

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,10 @@ default['qmail']['qmail_service'] = '/service'
 default['qmail']['qmail_bals'] = '/data/mail'
 default['qmail']['ldapuid'] = '1007'
 default['qmail']['ldapgid'] = '104'
+
+default['qmail']['ldapserver'] = 'localhost'
+default['qmail']['ldapbasedn'] = 'dc=example'
+default['qmail']['ldaplogin'] = "cn=manager,#{node['qmail']['ldapbasedn']}"
+default['qmail']['ldapgrouplogin'] = "cn=manager,#{node['qmail']['ldapbasedn']}"
+default['qmail']['ldappassword'] = 'password'
+default['qmail']['ldapgrouppassword'] = 'password'
